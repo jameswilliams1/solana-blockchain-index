6bb50ed147932b991a65bdd93f635fdf75620afb
“luke4u” <vistaxjtu@gmail.com>
2022-05-20 15:35:19 +0100

    run formatting

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,26 @@
 use pyth_sdk_solana::{load_price_feed_from_account, Price};
-use solana_program::pubkey::Pubkey;
 use solana_client::rpc_client::RpcClient;
-use std::str::FromStr;
+use solana_program::pubkey::Pubkey;
 use std::option::Option;
+use std::str::FromStr;
 
 extern crate chrono;
 use chrono::prelude::DateTime;
 use chrono::Utc;
-use std::time::{UNIX_EPOCH, Duration};
+use std::time::{Duration, UNIX_EPOCH};
 
 fn read_price(key: &String, url: &String) -> f64 {
-    
     let clnt = RpcClient::new(url.to_string());
     let price_key = Pubkey::from_str(key).unwrap();
 
     let mut price_account = clnt.get_account(&price_key).unwrap();
-    let price_feed =
-            load_price_feed_from_account(&price_key, &mut price_account).unwrap();
+    let price_feed = load_price_feed_from_account(&price_key, &mut price_account).unwrap();
     let current_price: Option<Price> = price_feed.get_current_price();
-    
+
     if current_price.is_none() {
         return -0.0001;
-    } 
-    
+    }
+
     let publish_time = price_feed.publish_time as u64;
     let publish_time_str = convert_timestamp(publish_time);
     let expo = current_price.unwrap().expo;
@@ -30,8 +28,14 @@ fn read_price(key: &String, url: &String) -> f64 {
     let multipled_price: f64 = current_price.unwrap().price as f64;
 
     let price: f64 = multipled_price * base.powi(expo);
-    
-    println!("price: ({} +- {}) x 10^{} at {}", current_price.unwrap().price, current_price.unwrap().conf, current_price.unwrap().expo, publish_time_str);
+
+    println!(
+        "price: ({} +- {}) x 10^{} at {}",
+        current_price.unwrap().price,
+        current_price.unwrap().conf,
+        current_price.unwrap().expo,
+        publish_time_str
+    );
 
     price
 }
@@ -63,9 +67,9 @@ fn consume_price_off_chain() {
     let doge_key = String::from("FsSM3s38PX9K7Dn6eGzuE29S2Dsk1Sss1baytTQdCaQj");
     let dot_key = String::from("EcV1X1gY2yb4KXxjVQtTHTbioum2gvmPnFk4zYAt7zne");
 
-    loop {        
+    loop {
         let mut price_array: [f64; 10] = [0.0; 10];
-        let mut avg_price: f64 = 0.0;
+
         let btc_price = read_price(&btc_key, &url);
         let eth_price = read_price(&eth_key, &url);
         let usdt_price = read_price(&usdt_key, &url);
@@ -87,13 +91,12 @@ fn consume_price_off_chain() {
         price_array[8] = doge_price;
         price_array[9] = dot_price;
 
-        avg_price = average(&price_array);
+        let avg_price: f64 = average(&price_array);
         println!("current price: {:?}", price_array);
         println!("current average price: {}", avg_price);
-
     }
 }
 
-fn main(){
+fn main() {
     consume_price_off_chain()
 }
