d6a9311bd267fd33fa883921001b5612456708f1
James Williams <jamesleighwilliams@gmail.com>
2022-05-24 21:37:40 +0100

    add account to load btc price

--- programs/solana_blockchain_index/src/lib.rs
+++ programs/solana_blockchain_index/src/lib.rs
@@ -1,22 +1,29 @@
 use anchor_lang::prelude::*;
+mod pyth_service;
 
 declare_id!("4NRxSxxnNA9xKHKrmj9RD16pmGRTDMvp3SkY4uB7ifdM");
 
 const DISCRIMINATOR_LENGTH: usize = 8;
 const TIMESTAMP_LENGTH: usize = 8;
 const VALUE_LENGTH: usize = 8;
+const EXPO_LENGTH: usize = 4;
+const CONF_LENGTH: usize = 8;
 
 #[account]
 #[derive(Debug)]
 pub struct IndexValue {
     pub timestamp: i64,
-    pub value: f64,
+    pub value: i64,
+    pub expo: i32,
+    pub conf: u64,
 }
 
 impl IndexValue {
     const LEN: usize = DISCRIMINATOR_LENGTH // added by Solana
         + TIMESTAMP_LENGTH // timestamp
-        + VALUE_LENGTH; // value
+        + VALUE_LENGTH // value
+        + EXPO_LENGTH // exponent
+        + CONF_LENGTH; // confidence
 }
 
 #[program]
@@ -28,14 +35,12 @@ pub mod solana_blockchain_index {
         let clock: Clock = Clock::get().unwrap();
 
         index_value.timestamp = clock.unix_timestamp;
-        // TODO get value from pyth here
-        index_value.value = 0.0;
+        let btc_value = pyth_service::price_of_account(&ctx.accounts.btc_account);
+        index_value.value = btc_value.price;
+        index_value.conf = btc_value.conf;
+        index_value.expo = btc_value.expo;
 
-        msg!(
-            "Index value at {} is {}",
-            index_value.timestamp,
-            index_value.value
-        );
+        msg!("New index value: {:?}", index_value);
         Ok(())
     }
 }
@@ -47,4 +52,6 @@ pub struct UpdateIndexValue<'info> {
     #[account(mut)]
     pub user: Signer<'info>,
     pub system_program: Program<'info, System>,
+    /// CHECK should be validated at read time
+    pub btc_account: AccountInfo<'info>,
 }
---
+++ programs/solana_blockchain_index/src/pyth_service.rs
@@ -0,0 +1,18 @@
+use anchor_lang::prelude::*;
+use pyth_sdk_solana::{load_price_feed_from_account_info, Price, PriceFeed};
+
+pub fn price_of_account(price_account_info: &AccountInfo) -> Price {
+    let price_feed: PriceFeed = load_price_feed_from_account_info(price_account_info).unwrap();
+    let current_price: Price = price_feed.get_current_price().unwrap();
+
+    msg!("{:?}", price_feed);
+    msg!(
+        "Price for {} at {} is ({} +- {}) x 10^{}",
+        price_feed.product_id,
+        price_feed.publish_time,
+        current_price.price,
+        current_price.conf,
+        current_price.expo,
+    );
+    current_price
+}
