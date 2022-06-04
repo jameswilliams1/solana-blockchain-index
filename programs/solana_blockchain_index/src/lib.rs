a1787091f7dee57b7a8ed312931fd699df447f33
James Williams <jamesleighwilliams@gmail.com>
2022-06-04 01:05:23 +0100

    refactor index account to use PDA

--- programs/solana_blockchain_index/src/lib.rs
+++ programs/solana_blockchain_index/src/lib.rs
@@ -9,6 +9,7 @@ const TIME_LENGTH: usize = 8;
 const PRICE_LENGTH: usize = 8;
 const EXPO_LENGTH: usize = 4;
 const CONF_LENGTH: usize = 8;
+const BUMP_LENGTH: usize = 1;
 
 #[account]
 #[derive(Debug)]
@@ -17,6 +18,7 @@ pub struct IndexValue {
     pub expo: i32,  // exponent (decimal_price = price x 10^expo)
     pub conf: u64,  // confidence (+-)
     pub time: i64,  // unix timestamp
+    pub bump: u8,   // PDA bump (makes it easier to find the PDA later)
 }
 
 impl IndexValue {
@@ -24,12 +26,22 @@ impl IndexValue {
         + TIME_LENGTH // timestamp
         + PRICE_LENGTH // price
         + EXPO_LENGTH // exponent
+        + BUMP_LENGTH // bump
         + CONF_LENGTH; // confidence
 }
 
 #[program]
 pub mod solana_blockchain_index {
     use super::*;
+
+    /**
+     * Create a new PDA for the current user to store the index value.
+     */
+    pub fn initialise(ctx: Context<Initialise>) -> Result<()> {
+        let index_value: &mut Account<IndexValue> = &mut ctx.accounts.index_value;
+        index_value.bump = *ctx.bumps.get("index_value").unwrap();
+        Ok(())
+    }
 
     pub fn update_index_value(ctx: Context<UpdateIndexValue>) -> Result<()> {
         let index_value: &mut Account<IndexValue> = &mut ctx.accounts.index_value;
@@ -61,11 +73,16 @@ pub mod solana_blockchain_index {
 
 #[derive(Accounts)]
 pub struct UpdateIndexValue<'info> {
-    #[account(init, payer = user, space = IndexValue::LEN)]
+    #[account(
+        mut,
+        seeds = [b"index_value", user.key().as_ref()],
+        bump = index_value.bump,
+    )]
     pub index_value: Account<'info, IndexValue>,
-    #[account(mut)]
+
     pub user: Signer<'info>,
     pub system_program: Program<'info, System>,
+
     /// CHECK should be validated at read time
     pub bnb_account: AccountInfo<'info>,
     /// CHECK should be validated at read time
@@ -76,4 +93,21 @@ pub struct UpdateIndexValue<'info> {
     pub eth_account: AccountInfo<'info>,
     /// CHECK should be validated at read time
     pub ltc_account: AccountInfo<'info>,
+}
+
+#[derive(Accounts)]
+pub struct Initialise<'info> {
+    #[account(
+        init,
+        payer = user,
+        space = IndexValue::LEN,
+        seeds = [b"index_value", user.key().as_ref()],
+        bump,
+    )]
+    pub index_value: Account<'info, IndexValue>,
+
+    #[account(mut)]
+    pub user: Signer<'info>,
+
+    pub system_program: Program<'info, System>,
 }
