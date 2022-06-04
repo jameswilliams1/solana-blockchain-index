f7558af5838be3784b9c3bdd79bc542673df4631
Luke <45267576+luke4u@users.noreply.github.com>
2022-06-04 01:29:07 +0100

    Add more tokens (#8)
    * update program id
    
    * added 8 more tokens
    
    * fixed conflict in archor.toml
    
    * add anchor log to git
    
    * use 5 tokens for index value
    
    * fixup after rebase
    
    * refactor index account to use PDA
    
    * update program id
    
    Co-authored-by: James Williams <jamesleighwilliams@gmail.com>

--- programs/solana_blockchain_index/src/lib.rs
+++ programs/solana_blockchain_index/src/lib.rs
@@ -2,13 +2,14 @@ use anchor_lang::prelude::*;
 mod index_service;
 mod pyth_service;
 
-declare_id!("7kARzULggbgDSLLpBgERNHQSpkse1qM3RB4NEEvS3ztu");
+declare_id!("56yNRQWKqpJdPSMDum7TCr7evVLt1uzXZkrarBZowyXz");
 
 const DISCRIMINATOR_LENGTH: usize = 8;
 const TIME_LENGTH: usize = 8;
 const PRICE_LENGTH: usize = 8;
 const EXPO_LENGTH: usize = 4;
 const CONF_LENGTH: usize = 8;
+const BUMP_LENGTH: usize = 1;
 
 #[account]
 #[derive(Debug)]
@@ -17,6 +18,7 @@ pub struct IndexValue {
     pub expo: i32,  // exponent (decimal_price = price x 10^expo)
     pub conf: u64,  // confidence (+-)
     pub time: i64,  // unix timestamp
+    pub bump: u8,   // PDA bump (makes it easier to find the PDA later)
 }
 
 impl IndexValue {
@@ -24,6 +26,7 @@ impl IndexValue {
         + TIME_LENGTH // timestamp
         + PRICE_LENGTH // price
         + EXPO_LENGTH // exponent
+        + BUMP_LENGTH // bump
         + CONF_LENGTH; // confidence
 }
 
@@ -31,11 +34,27 @@ impl IndexValue {
 pub mod solana_blockchain_index {
     use super::*;
 
+    /**
+     * Create a new PDA for the current user to store the index value.
+     */
+    pub fn initialise(ctx: Context<Initialise>) -> Result<()> {
+        let index_value: &mut Account<IndexValue> = &mut ctx.accounts.index_value;
+        index_value.bump = *ctx.bumps.get("index_value").unwrap();
+        Ok(())
+    }
+
     pub fn update_index_value(ctx: Context<UpdateIndexValue>) -> Result<()> {
         let index_value: &mut Account<IndexValue> = &mut ctx.accounts.index_value;
         let clock: Clock = Clock::get().unwrap();
 
-        let price_accounts = vec![&ctx.accounts.btc_account, &ctx.accounts.eth_account];
+        let price_accounts = vec![
+            &ctx.accounts.bnb_account,
+            &ctx.accounts.btc_account,
+            &ctx.accounts.doge_account,
+            &ctx.accounts.eth_account,
+            &ctx.accounts.ltc_account,
+        ];
+
         let prices: Vec<_> = price_accounts
             .iter()
             .map(|pa| pyth_service::price_of_account(pa))
@@ -54,13 +73,41 @@ pub mod solana_blockchain_index {
 
 #[derive(Accounts)]
 pub struct UpdateIndexValue<'info> {
-    #[account(init, payer = user, space = IndexValue::LEN)]
+    #[account(
+        mut,
+        seeds = [b"index_value", user.key().as_ref()],
+        bump = index_value.bump,
+    )]
     pub index_value: Account<'info, IndexValue>,
-    #[account(mut)]
+
     pub user: Signer<'info>,
     pub system_program: Program<'info, System>,
+
+    /// CHECK should be validated at read time
+    pub bnb_account: AccountInfo<'info>,
     /// CHECK should be validated at read time
     pub btc_account: AccountInfo<'info>,
     /// CHECK should be validated at read time
+    pub doge_account: AccountInfo<'info>,
+    /// CHECK should be validated at read time
     pub eth_account: AccountInfo<'info>,
+    /// CHECK should be validated at read time
+    pub ltc_account: AccountInfo<'info>,
+}
+
+#[derive(Accounts)]
+pub struct Initialise<'info> {
+    #[account(
+        init,
+        payer = user,
+        space = IndexValue::LEN,
+        seeds = [b"index_value", user.key().as_ref()],
+        bump,
+    )]
+    pub index_value: Account<'info, IndexValue>,
+
+    #[account(mut)]
+    pub user: Signer<'info>,
+
+    pub system_program: Program<'info, System>,
 }
--- tests/solana_blockchain_index.ts
+++ tests/solana_blockchain_index.ts
@@ -1,46 +1,87 @@
 import * as anchor from "@project-serum/anchor";
 import { Program } from "@project-serum/anchor";
 import { expect } from "chai";
+import { strict as assert } from "assert";
+import { PublicKey } from "@solana/web3.js";
 import { SolanaBlockchainIndex } from "../target/types/solana_blockchain_index";
 
-describe("SolanaBlockchainIndex", () => {
+describe("SolanaBlockchainIndex", async () => {
   // Configure the client to use the local cluster.
   anchor.setProvider(anchor.AnchorProvider.env());
 
   const program = anchor.workspace
     .SolanaBlockchainIndex as Program<SolanaBlockchainIndex>;
-  const programProvider = program.provider as anchor.AnchorProvider;
+  const provider = program.provider as anchor.AnchorProvider;
 
-  it("Writes calculated index value and timestamp to account", async () => {
-    const keypair = anchor.web3.Keypair.generate();
-    console.log(
-      `Requesting index calculation for new account: ${keypair.publicKey} owned by user ${programProvider.wallet.publicKey}`
-    );
-    // set off new index calculation
-    const tx = await program.methods
-      .updateIndexValue()
-      .accounts({
-        indexValue: keypair.publicKey,
-        user: programProvider.wallet.publicKey,
-        systemProgram: anchor.web3.SystemProgram.programId,
-        btcAccount: "GVXRSBjFk6e6J3NbVPXohDJetcTjaeeuykUpbQF8UoMU",
-        ethAccount: "JBu1AL4obBcCMqKBBxhpWCNUt136ijcuMZLFvTP7iWdB",
-      })
-      .signers([keypair])
-      .rpc();
-    console.log(`Calculation finished. Transaction signature: ${tx}`);
+  const [indexValuePda, _] = await PublicKey.findProgramAddress(
+    [
+      anchor.utils.bytes.utf8.encode("index_value"),
+      provider.wallet.publicKey.toBuffer(),
+    ],
+    program.programId
+  );
 
-    // verify result
-    console.log(`Fetching account: ${keypair.publicKey}`);
-    const indexValue = await program.account.indexValue.fetch(
-      keypair.publicKey
-    );
-    console.log(
-      `Account contents:\n\tprice: ${indexValue.price}\n\texpo: ${indexValue.expo}\n\tconf: ${indexValue.conf}\n\ttime: ${indexValue.time}`
-    );
-    expect(indexValue.time).is.instanceOf(anchor.BN);
-    expect(indexValue.price.toNumber()).to.equal(1567878500000);
-    expect(indexValue.conf.toNumber()).to.equal(512203289);
-    expect(indexValue.expo).to.equal(-8);
+  describe("initialise", async () => {
+    it("Does not initialise PDA using incorrect address", async () => {
+      const keypair = anchor.web3.Keypair.generate();
+      assert.rejects(
+        program.methods
+          .initialise()
+          .accounts({
+            indexValue: keypair.publicKey, // does not match PDA
+            user: provider.wallet.publicKey,
+            systemProgram: anchor.web3.SystemProgram.programId,
+          })
+          .rpc()
+      );
+    });
+
+    it("Initialises PDA using correct address", async () => {
+      await program.methods
+        .initialise()
+        .accounts({
+          indexValue: indexValuePda, // fixed value derived from programId and user.key()
+          user: provider.wallet.publicKey,
+          systemProgram: anchor.web3.SystemProgram.programId,
+        })
+        .rpc();
+
+      const indexValue = await program.account.indexValue.fetch(indexValuePda);
+      expect(indexValue.bump).is.not.null; // should be initialised to use when updating later
+    });
+  });
+
+  describe("updateIndexValue", async () => {
+    it("Writes calculated index value and timestamp to account", async () => {
+      console.log(
+        `Requesting index calculation for existing account: ${indexValuePda} owned by user ${provider.wallet.publicKey}`
+      );
+      // set off new index calculation
+      const tx = await program.methods
+        .updateIndexValue()
+        .accounts({
+          indexValue: indexValuePda,
+          user: provider.wallet.publicKey,
+          systemProgram: anchor.web3.SystemProgram.programId,
+          bnbAccount: "4CkQJBxhU8EZ2UjhigbtdaPbpTe6mqf811fipYBFbSYN",
+          btcAccount: "GVXRSBjFk6e6J3NbVPXohDJetcTjaeeuykUpbQF8UoMU",
+          dogeAccount: "FsSM3s38PX9K7Dn6eGzuE29S2Dsk1Sss1baytTQdCaQj",
+          ethAccount: "JBu1AL4obBcCMqKBBxhpWCNUt136ijcuMZLFvTP7iWdB",
+          ltcAccount: "8RMnV1eD55iqUFJLMguPkYBkq8DCtx81XcmAja93LvRR",
+        })
+        .rpc();
+      console.log(`Calculation finished. Transaction signature: ${tx}`);
+
+      // verify result
+      console.log(`Fetching account: ${indexValuePda}`);
+      const indexValue = await program.account.indexValue.fetch(indexValuePda);
+      console.log(
+        `Account contents:\n\tprice: ${indexValue.price}\n\texpo: ${indexValue.expo}\n\tconf: ${indexValue.conf}\n\ttime: ${indexValue.time}`
+      );
+      expect(indexValue.time).is.instanceOf(anchor.BN);
+      expect(indexValue.price.toNumber()).to.equal(634453012372);
+      expect(indexValue.conf.toNumber()).to.equal(204991922);
+      expect(indexValue.expo).to.equal(-8);
+    });
   });
 });
