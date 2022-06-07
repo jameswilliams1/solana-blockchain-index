a2e5b62b18e24e53a9b4ab8bb0511beeb72e8789
James Williams <jamesleighwilliams@gmail.com>
2022-06-07 14:56:28 +0100

    handle index init in update script

--- app/src/update_index_value.ts
+++ app/src/update_index_value.ts
@@ -1,55 +1,90 @@
 /**
- * Update the index value on the Solana DevNet.
+ * Update the index value on the Blockchain.
  *
- * Must set env var ANCHOR_WALLET to wallet path before calling.
+ * Must set env vars:
+ *   ANCHOR_WALLET wallet path
+ *   CLUSTER_URL e.g. https://api.devnet.solana.com
+ *
+ * Args:
+ *   --init also initialise the index account for the first time
  */
 import * as anchor from "@project-serum/anchor";
 import { Program } from "@project-serum/anchor";
 import { PublicKey } from "@solana/web3.js";
 import { SolanaBlockchainIndex } from "./idl/solana_blockchain_index";
 
+const urlToAccounts = {
+  "http://localhost:8899": {
+    bnbAccount: "4CkQJBxhU8EZ2UjhigbtdaPbpTe6mqf811fipYBFbSYN",
+    btcAccount: "GVXRSBjFk6e6J3NbVPXohDJetcTjaeeuykUpbQF8UoMU",
+    dogeAccount: "FsSM3s38PX9K7Dn6eGzuE29S2Dsk1Sss1baytTQdCaQj",
+    ethAccount: "JBu1AL4obBcCMqKBBxhpWCNUt136ijcuMZLFvTP7iWdB",
+    ltcAccount: "8RMnV1eD55iqUFJLMguPkYBkq8DCtx81XcmAja93LvRR",
+  },
+  "https://api.devnet.solana.com": {
+    bnbAccount: "GwzBgrXb4PG59zjce24SF2b9JXbLEjJJTBkmytuEZj1b",
+    btcAccount: "HovQMDrbAgAYPCmHVSrezcSmkMtXSSUsLDFANExrZh2J",
+    dogeAccount: "4L6YhY8VvUgmqG5MvJkUJATtzB2rFqdrJwQCmFLv4Jzy",
+    ethAccount: "EdVCmQ9FSPcVe5YySXDPCRmc8aDQLKJ9xvYBMZPie1Vw",
+    ltcAccount: "BLArYBCUYhdWiY8PCUTpvFE21iaJq85dvxLk9bYMobcU",
+  },
+};
+
 const main = async () => {
-  anchor.setProvider(
-    anchor.AnchorProvider.local("https://api.devnet.solana.com")
-  );
+  const clusterUrl = process.env.CLUSTER_URL;
+  anchor.setProvider(anchor.AnchorProvider.local(clusterUrl));
+  const priceAccounts = urlToAccounts[clusterUrl];
 
   const program = anchor.workspace
     .SolanaBlockchainIndex as Program<SolanaBlockchainIndex>;
   const provider = program.provider as anchor.AnchorProvider;
 
-  const [indexValuePda, _] = await PublicKey.findProgramAddress(
-    [
-      anchor.utils.bytes.utf8.encode("index_value"),
-      provider.wallet.publicKey.toBuffer(),
-    ],
+  const user = provider.wallet.publicKey;
+  const systemProgram = anchor.web3.SystemProgram.programId;
+  const [indexValue, _] = await PublicKey.findProgramAddress(
+    [anchor.utils.bytes.utf8.encode("index_value"), user.toBuffer()],
     program.programId
   );
 
-  // PDA should be initialised already
+  // init index account
+  if (process.argv.includes("--init")) {
+    console.log(`Creating new index account ${indexValue}`);
+    const tx = await program.methods
+      .initialise()
+      .accounts({
+        indexValue,
+        user,
+        systemProgram,
+      })
+      .rpc();
+    console.log(`Transaction signature: ${tx}`);
+  }
+
+  // actual index update
   console.log(
-    `Requesting index calculation for existing account: ${indexValuePda} owned by user ${provider.wallet.publicKey}`
+    `Performing index calculation for account ${indexValue} owned by user ${user}`
   );
   // set off new index calculation
   const tx = await program.methods
     .updateIndexValue()
     .accounts({
-      indexValue: indexValuePda,
-      user: provider.wallet.publicKey,
-      systemProgram: anchor.web3.SystemProgram.programId,
-      // devnet addresses
-      bnbAccount: "GwzBgrXb4PG59zjce24SF2b9JXbLEjJJTBkmytuEZj1b",
-      btcAccount: "HovQMDrbAgAYPCmHVSrezcSmkMtXSSUsLDFANExrZh2J",
-      dogeAccount: "4L6YhY8VvUgmqG5MvJkUJATtzB2rFqdrJwQCmFLv4Jzy",
-      ethAccount: "EdVCmQ9FSPcVe5YySXDPCRmc8aDQLKJ9xvYBMZPie1Vw",
-      ltcAccount: "BLArYBCUYhdWiY8PCUTpvFE21iaJq85dvxLk9bYMobcU",
+      indexValue,
+      user,
+      systemProgram,
+      ...priceAccounts,
     })
     .rpc();
-  console.log(`Calculation finished. Transaction signature: ${tx}`);
-  console.log(`Fetching account: ${indexValuePda}`);
-  const indexValue = await program.account.indexValue.fetch(indexValuePda);
+  console.log(`Transaction signature: ${tx}`);
+  const indexValueData = await program.account.indexValue.fetch(indexValue);
   console.log(
-    `Account contents:\n\tprice: ${indexValue.price}\n\texpo: ${indexValue.expo}\n\tconf: ${indexValue.conf}\n\ttime: ${indexValue.time}`
+    `Contents of account ${indexValue}:
+    \tprice: ${indexValueData.price}
+    \texpo: ${indexValueData.expo}
+    \tconf: ${indexValueData.conf}
+    \ttime: ${indexValueData.time}`
   );
+  console.log("SUCCESS\n");
+  process.exit(0);
 };
 
 main().catch((err) => {
